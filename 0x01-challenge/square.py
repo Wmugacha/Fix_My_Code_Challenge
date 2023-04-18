6627841288f7af856a3109eb02e31e58a7aa2828
Wmugacha <wilfredmugacha@gmail.com>
2023-04-18 18:04:03 +0000

    perfect square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+""" Class that defines a square """
 
 
-class square():
+class Square:
 
     width = 0
     height = 0
@@ -26,7 +27,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
